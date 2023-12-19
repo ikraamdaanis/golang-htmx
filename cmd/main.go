0d72b29a5c243a309b51c0acfcafb23653f8cbff
Ikraam <ibndaanis@gmail.com>
2023-12-19 09:05:30 +0000

    echo server with html pages

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello world.")
+	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+
+	e.Renderer = t
+
+	e.Static("/", "assets")
+	e.GET("/", Homepage)
+	e.GET("/hello", Hello)
+
+	e.Logger.Fatal(e.Start(":5000"))
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Homepage(c echo.Context) error {
+	return c.Render(http.StatusOK, "index", "World")
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
 }
