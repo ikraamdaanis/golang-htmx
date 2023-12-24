5f1d1d36d13fe4f3f6964b3aeecb578aeb2cc66c
Ikraam <ibndaanis@gmail.com>
2023-12-24 19:32:54 +0000

    added remaining fields

--- cmd/main.go
+++ cmd/main.go
@@ -36,13 +36,20 @@ func Hello(c echo.Context) error {
 }
 
 type FormData struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Published   bool   `json:"published"`
+	Content     string `json:"content"`
+	Description string `json:"description"`
+	UserId      string `json:"user_id"`
+	Views       int    `json:"views"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
 	// Add more fields based on your database schema
 }
 
 func Test(c echo.Context) error {
-	rows, _ := initializers.DB.Query("SELECT id, name FROM forms")
+	rows, _ := initializers.DB.Query("SELECT id, name, published, content, description, user_id, views, created_at, updated_at FROM forms")
 
 	defer rows.Close()
 
@@ -50,7 +57,7 @@ func Test(c echo.Context) error {
 
 	for rows.Next() {
 		var formData FormData
-		err := rows.Scan(&formData.ID, &formData.Name)
+		err := rows.Scan(&formData.ID, &formData.Name, &formData.Published, &formData.Content, &formData.Description, &formData.UserId, &formData.Views, &formData.CreatedAt, &formData.UpdatedAt)
 		if err != nil {
 			log.Fatal(err)
 		}
